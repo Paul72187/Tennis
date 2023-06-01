cfdf3e0bb74739c47db4bb61d3ece196c20d95d8
Paul Cooper <87516199+Paul72187@users.noreply.github.com>
2023-06-01 15:21:22 +0100

    Create play_tennis.py

---
+++ play_tennis.py
@@ -0,0 +1,8 @@
+from tennis import Player, Match
+
+nadal = Player("Rafael Nadal", 2000)
+djokovic = Player("Novak Djokovic", 2000)
+
+print(nadal.name)
+print(nadal.ranking_points)
+print(djokovic.name)
