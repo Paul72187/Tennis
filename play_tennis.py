30b7ccdd1676dfb127565d8b230471cf799345ee
Paul72187 <paulcooper@Pauls-MacBook-Air.local>
2023-06-01 17:07:55 +0100

    Test set score added

--- play_tennis.py
+++ play_tennis.py
@@ -6,4 +6,7 @@ djokovic = Player("Novak Djokovic", 2000)
 test_match = Match(nadal, djokovic)
 test_set = Set(test_match)
 
-test_set.play_game()
+while test_set.is_running():
+    test_set.play_game()
+    print(str(test_set))
+print(test_set.winner)
