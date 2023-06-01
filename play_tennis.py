a4617a8f72b16bccaedb30aea1d7681908b26693
Paul72187 <paulcooper@Pauls-MacBook-Air.local>
2023-06-01 18:13:36 +0100

    Match testing added

--- play_tennis.py
+++ play_tennis.py
@@ -1,12 +1,10 @@
-from tennis import Player, Match, Set
+from tennis import Player, Match
 
 nadal = Player("Rafael Nadal", 2000)
 djokovic = Player("Novak Djokovic", 2000)
 
 test_match = Match(nadal, djokovic)
-test_set = Set(test_match)
 
-while test_set.is_running():
-    test_set.play_game()
-    print(str(test_set))
-print(test_set.winner)
+while test_match.is_running():
+    test_match.play_set()
+
